57aa63e6f0e9554ec738671192ab9720648af8db
Aditi Shrivastava <aditishrivastava1520@gmail.com>
2026-07-01 13:27:37 +0530

    Time: 34 ms (41.91%), Space: 47.2 MB (23.11%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,16 @@
+class Solution { //two pointers lgega qki 2 element ki baat hori
+    public int[] twoSum(int[] nums, int target) {
+        int n= nums.length;
+        int ans[] = new int[2];
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i] + nums[j] == target){
+                ans[0] = i;
+                ans[1] = j;
+                break;
+                }
+            }
+        }
+       return ans;
+    }
+}
