847300acca77aefc2488dc0949a6054672764a12
Aditi Shrivastava <aditishrivastava1520@gmail.com>
2026-07-13 19:55:53 +0530

    Time: 32 ms (51.6%), Space: 76.8 MB (43%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] copy = arr.clone(); //ek copy arr banao taki usme rank store kre
+        Arrays.sort(copy);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int rank=1;
+        for(int i =0; i<copy.length; i++){
+            if(map.containsKey(copy[i]) == false){
+                map.put(copy[i] , rank++);
+            }
+        }
+        for(int i =0; i<arr.length; i++){
+            arr[i] = map.get(arr[i]);
+        }
+        return arr;
+    }
+}
